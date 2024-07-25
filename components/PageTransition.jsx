34dd34c723e616b229ce614858dc978df53f0192
jasonfaeq <jasonfaeq@gmail.com>
2024-07-25 16:36:13 +0200

    added

---
+++ components/PageTransition.jsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { AnimatePresence, motion } from "framer-motion";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+const PageTransition = ({ children }) => {
+  const pathname = usePathname();
+
+  return (
+    <AnimatePresence mode="wait">
+      <motion.div
+        key={pathname}
+        initial={{ opacity: 1 }}
+        animate={{
+          opacity: 0,
+          transition: { delay: 1, duration: 0.4, ease: "easeInOut" },
+        }}
+        exit={{ opacity: 1 }}
+        className="h-screen w-screen fixed bg-primary top-0 pointer-events-none"
+      />
+      {children}
+    </AnimatePresence>
+  );
+};
+
+export default PageTransition;
